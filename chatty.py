6de8d8b2e4253060048d12f79ecae86ed3bd6d6a
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-01-27 01:26:10 -0600

    Theoretically working

--- chatty.py
+++ chatty.py
@@ -6,41 +6,58 @@ import logging
 import markovify
 import schedule
 from os import environ
+from random import randrange
 
 CHATTY_BOT_TOKEN = environ['CHATTY_BOT_TOKEN']
 CHATTY_CHAT_ID = environ['CHATTY_CHAT_ID']
 
-MODEL_NAME = "model.json"
+MODEL_NAME = "text_model.json"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-# TODO: needs type hinting
-def save_model(corpus) -> None:
+def save_model() -> None:
     with open(MODEL_NAME, "w") as f:
-        f.write(markovify.Text(corpus).to_json())
+        f.write(markovify.Text(text_model).to_json())
 
 def load_model() -> markovify.Text:
     with open(MODEL_NAME, "r") as f:
         return markovify.Text.from_json(f.read())
 
-# TODO: needs type hinting
-def train(message: String, corpus) -> None:
+def train(message: String) -> None:
     schedule.run_pending()
-    return 0
 
+    new_text = markovify.Text(message)
+
+    if not text_model:
+        global text_model = new_text
+    else:
+        global text_model = markovify.combine([text_model, new_text], [1, 1.025])
+
+ 
 def reply(update: Update, context: CallbackContext) -> None:
     if str(update.message.chat_id) == str(CHATTY_CHAT_ID):
-        context.bot.send_chat_action(chat_id = update.effective_message.chat, action=ChatAction.TYPING)
+        context.bot.send_chat_action(chat_id = update.effective_message.chat, action = ChatAction.TYPING)
+        train(context.message.text)
 
+        full_message = ""
+        for i in randrange(5):
+            full_message += f"{text_model.make_sentence()} "
 
-
+        update.message.reply_text(full_message)
 
 
 if __name__ == "__main__":
-    corpus = None
-    schedule.every(10).minutes.do(save_model(corpus))
+    try:
+        text_model = load_model()
+    except:
+        text_model = None
+    
+    schedule.every(10).minutes.do(save_model(text_model))
 
     updater = Updater(CHATTY_BOT_TOKEN, use_context=True)
     dispacher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(Filters.text), train_and_reply)
+    dispatcher.add_handler(MessageHandler(Filters.text), reply)
+
+    updater.start_polling()
+    updater.idle()

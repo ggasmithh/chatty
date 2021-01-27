5eb5cf8e4a9730bab950f5ae199175798a1db7c9
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-01-27 00:58:23 -0600

    Bot sends TYPING action while thinking

--- chatty.py
+++ chatty.py
@@ -1,7 +1,7 @@
 from telegram import ChatAction, Update
 from telegram
 from telegram import ChatAction, Update
-from telegram.ext import CommandHandler, Updater, CallbackContext
+from telegram.ext import MessageHandler, Filters, Updater, CallbackContext
 import logging
 import markovify
 import schedule
@@ -12,6 +12,10 @@ CHATTY_CHAT_ID = environ['CHATTY_CHAT_ID']
 
 MODEL_NAME = "model.json"
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+# TODO: needs type hinting
 def save_model(corpus) -> None:
     with open(MODEL_NAME, "w") as f:
         f.write(markovify.Text(corpus).to_json())
@@ -19,11 +23,24 @@ def save_model(corpus) -> None:
 def load_model() -> markovify.Text:
     with open(MODEL_NAME, "r") as f:
         return markovify.Text.from_json(f.read())
-    
+
+# TODO: needs type hinting
+def train(message: String, corpus) -> None:
+    schedule.run_pending()
+    return 0
+
+def reply(update: Update, context: CallbackContext) -> None:
+    if str(update.message.chat_id) == str(CHATTY_CHAT_ID):
+        context.bot.send_chat_action(chat_id = update.effective_message.chat, action=ChatAction.TYPING)
+
+
+
+
 
 if __name__ == "__main__":
     corpus = None
     schedule.every(10).minutes.do(save_model(corpus))
-    
-    # this will be invoked when a new text is added to the corpus
-    #schedule.run_pending() 
+
+    updater = Updater(CHATTY_BOT_TOKEN, use_context=True)
+    dispacher = updater.dispatcher
+    dispatcher.add_handler(MessageHandler(Filters.text), train_and_reply)

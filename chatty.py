eb79701ca4b9fefc851e185039245fdd0afd32c1
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-01-27 00:39:02 -0600

    Loading of saved model

--- chatty.py
+++ chatty.py
@@ -6,10 +6,20 @@ import logging
 import markovify
 import schedule
 
+MODEL_NAME = "model.json"
+
 def save_model(corpus) -> None:
-    with open("model.json", "w") as f:
+    with open(MODEL_NAME, "w") as f:
         f.write(markovify.Text(corpus).to_json())
+
+def load_model() -> markovify.Text:
+    with open(MODEL_NAME, "r") as f:
+        return markovify.Text.from_json(f.read())
+    
 
 if __name__ == "__main__":
     corpus = None
     schedule.every(10).minutes.do(save_model(corpus))
+    
+    # this will be invoked when a new text is added to the corpus
+    #schedule.run_pending() 

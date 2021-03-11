2413f9f6c2d0c65949ffafbb648addab2a1bed43
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-11 16:11:12 -0600

    Fixed case where a message is not a reply

--- chatty.py
+++ chatty.py
@@ -93,7 +93,12 @@ def train_and_reply(update: Update, context: CallbackContext) -> None:
     if check_chat_id(update) and not is_spam(update):
         train(update.message.text)
 
-        if randrange(0, 100) < 5 or update.message.reply_to_message.from_user.id == context.bot.get_me().id:
+        direct_reply = False
+        
+        if update.message.reply_to_message != None:
+            direct_reply = update.message.reply_to_message.from_user.id == context.bot.get_me().id
+
+        if randrange(0, 100) < 5 or direct_reply:
             context.bot.send_chat_action(chat_id = update.effective_message.chat_id, action = ChatAction.TYPING)
             full_message = ""
                               

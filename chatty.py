06d2ec2883fe827ae154b0ba13dedebb6176f484
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-11 14:50:22 -0600

    Added spam prevention

--- chatty.py
+++ chatty.py
@@ -12,10 +12,16 @@ CHATTY_CHAT_ID = environ['CHATTY_CHAT_ID']
 
 MODEL_NAME = "text_model.json"
 
+# If anyone tries to circumvent these limits I'm moving it to an environment var
+MESSAGE_LENGTH_LIMIT = 1000
+SAME_USER_LIMIT = 5
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-## HELPER FUNCTIONS ##
+####################
+# INTERNAL FUNCTIONS
+####################
 
 def save_model() -> None:
     if text_model:
@@ -36,8 +42,19 @@ def generate_get_size_message() -> str:
     setup_schedule()
     return output
 
-def check_chat(update: Update) -> bool:
+def check_chat_id(update: Update) -> bool:
     return str(update.message.chat_id) == str(CHATTY_CHAT_ID)
+
+def is_spam(update: Update) -> bool:
+    if update.message.from_user == messages_from_same_user['last_user']:
+        messages_from_same_user['count'] += 1
+        if messages_from_same_user['count'] > SAME_USER_LIMIT or len(update.message.text) > MESSAGE_LENGTH_LIMIT:
+            return True
+    
+    else:
+        messages_from_same_user['last_user'] = update.message.from_user
+        messages_from_same_user['last_user'] = 0
+        return False
 
 def train(message: str) -> None:
     schedule.run_pending()
@@ -51,14 +68,16 @@ def train(message: str) -> None:
     else:
         text_model = new_text
 
-## FUNCTIONS CALLED BY DISPATCHER ##
+################################### 
+# CHAT FUNCTIONS
+###################################
 
 def start(update: Update, context: CallbackContext) -> None:
-    if check_chat(update):
+    if check_chat_id(update):
         update.message.reply_text("Hello")
 
 def train_and_reply(update: Update, context: CallbackContext) -> None:
-    if check_chat(update):
+    if check_chat_id(update) and not is_spam(update):
         train(update.message.text)
 
         if randrange(0, 100) < 5:
@@ -73,33 +92,34 @@ def train_and_reply(update: Update, context: CallbackContext) -> None:
             update.message.reply_text(full_message)
 
 def get_size(update: Update, context: CallbackContext) -> None:
-    if check_chat(update):
+    if check_chat_id(update):
         context.bot.send_chat_action(chat_id = update.effective_message.chat_id, action = ChatAction.TYPING)
         update.message.reply_text(generate_get_size_message())
 
-if __name__ == "__main__":
-    try:
-        text_model = load_model()
-    except:
-        text_model = None
-    
-    setup_schedule()
-    atexit.register(save_model)
+##############
+# MAIN PROGRAM
+##############
+messages_from_same_user = {'last_user': None, 'count': 0}
 
-    updater = Updater(CHATTY_BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+try:
+    text_model = load_model()
+except:
+    text_model = None
 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+setup_schedule()
+atexit.register(save_model)
 
-    train_and_reply_handler = MessageHandler(Filters.text & (~Filters.command), train_and_reply)
-    dispatcher.add_handler(train_and_reply_handler)
+updater = Updater(CHATTY_BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-    get_size_handler = CommandHandler('get_size', get_size)
-    dispatcher.add_handler(get_size_handler)
-    
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
-    updater.start_polling()
-    updater.idle()
+train_and_reply_handler = MessageHandler(Filters.text & (~Filters.command), train_and_reply)
+dispatcher.add_handler(train_and_reply_handler)
 
+get_size_handler = CommandHandler('get_size', get_size)
+dispatcher.add_handler(get_size_handler)
 
+updater.start_polling()
+updater.idle()
